8a934aee9c94ea444e4189511c1f90f64e771b42
sahillede940 <sahillede940@gmail.com>
2024-12-04 22:28:09 +0530

    added gay realtors

--- utils/fetch_news.py
+++ utils/fetch_news.py
@@ -4,16 +4,14 @@ import requests
 
 
 def fetch_news(query):
-    """
-    Fetch news articles based on the query using Beautiful Soup.
-    """
-    # Perform a web search using the query
+
     URL = f"{MAIN_URL}/{query}".replace("\\", "/")
     gay_realtors_page = requests.get(URL)
 
     if gay_realtors_page.status_code == 200:
         soup = BeautifulSoup(gay_realtors_page.content, 'html.parser')
         news_soup = soup.find_all(class_="news_block")
+        gay_realtors_soup = soup.find_all(class_="agent_list_wrap")
 
         news = []
 
@@ -28,7 +26,38 @@ def fetch_news(query):
                 "description": description
             })
 
-        return news
+        gay_realtors = []
+        for realtor in gay_realtors_soup:
+            name = realtor.find("h3").find("a").text
+            name_url = realtor.find("h3").find("a")["href"]
+            agent_type = realtor.find("span", class_="agent_type").text
+            agent_type = agent_type.replace("-", "").strip()
+            stars = 5 if realtor.find("div", class_="agent_review").find(
+                "img") else None
+            profile_url = realtor.find("a", text="View Full Profile")["href"]
+            contact_url = realtor.find("a", text="Contact")["href"]
+            img_url = "https://www.gayrealestate.com" + \
+                realtor.find("div", class_="agent_small_pic").find(
+                    "img")["data-src"]
+            # description class agent_info inside p tag
+            description = " ".join([p.text for p in realtor.find(
+                "div", class_="agent_info").find_all("p")]).replace("...read more", "")
+
+            gay_realtors.append({
+                "name": name,
+                "description": description,
+                "name_url": name_url,
+                "agent_type": agent_type,
+                "stars": stars,
+                "profile_url": profile_url,
+                "contact_url": contact_url,
+                "img_url": img_url,
+            })
+
+        return {
+            "news": news,
+            "gay_realtors": gay_realtors
+        }
     else:
         print(
             f"Failed to fetch the page. Status code: {gay_realtors_page.status_code}")
